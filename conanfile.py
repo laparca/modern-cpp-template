b463401cb7654eabd0ae7b50d2b4d8264b6a1687
Samuel R. Sevilla <laparca@laparca.es>
2026-04-21 19:27:37 +0200

    accept all clangs

--- conanfile.py
+++ conanfile.py
@@ -7,12 +7,12 @@ class ProjectConan(ConanFile):
     requires = "gtest/1.17.0"
 
     def requirements(self):
-        if self.settings.os == "Macos" and self.settings.compiler == "apple-clang":
+        if self.settings.os == "Macos" and self.settings.compiler in ["apple-clang", "clang"]:
             self.requires("llvm-core/19.1.0")
 
     def generate(self):
         tc = CMakeToolchain(self)
-        if self.settings.os == "Macos" and self.settings.compiler == "apple-clang":
+        if self.settings.os == "Macos" and self.settings.compiler in ["apple-clang", "clang"]:
             llvm = self.dependencies["llvm-core"]
             tc.variables["CMAKE_C_COMPILER"] = f"{llvm.package_folder}/bin/clang"
             tc.variables["CMAKE_CXX_COMPILER"] = f"{llvm.package_folder}/bin/clang++"
